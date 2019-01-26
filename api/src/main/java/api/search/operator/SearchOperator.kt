6e4d57d4a4331254e47cf9c6cd5446ffcabc19a4
Dmitry <dimas1114@yandex.ru>
2019-01-26 14:37:11 +0300

    auditor aware test

--- api/src/main/java/api/search/operator/SearchOperator.kt
+++ api/src/main/java/api/search/operator/SearchOperator.kt
@@ -1,10 +1,15 @@
 package api.search.operator
 
 import com.querydsl.core.BooleanBuilder
+import com.querydsl.core.types.Path
+import com.querydsl.core.types.dsl.BooleanExpression
+import com.querydsl.core.types.dsl.DatePath
+import com.querydsl.core.types.dsl.NumberPath
 import com.querydsl.core.types.dsl.PathBuilder
 import org.springframework.core.convert.support.DefaultConversionService
 import org.springframework.data.querydsl.binding.QuerydslBindings
 import org.springframework.util.MultiValueMap
+import java.lang.IllegalArgumentException
 
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -22,43 +27,35 @@ enum class SearchOperator(val operator: String,
         booleanBuilder.and(pathBuilder.get(propertyPath).eq(value[0]))
     }),
     GT("gt", { booleanBuilder, pathBuilder, propertyPath, value, fieldType ->
-        value.forEach {
-            when(fieldType) {
-                longClass, integerClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, longClass).gt(it.toLong()))
-                doubleClass, floatClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, doubleClass).gt(it.toDouble()))
-            }
+        when (fieldType) {
+            longClass, integerClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, longClass).gt(value[0].toLong()))
+            doubleClass, floatClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, doubleClass).gt(value[0].toDouble()))
         }
     }),
     LT("lt", { booleanBuilder, pathBuilder, propertyPath, value, fieldType ->
-        value.forEach {
-            when(fieldType){
-                longClass, integerClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, longClass).lt(it.toLong()))
-                doubleClass, floatClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, doubleClass).lt(it.toDouble()))
-            }
+        when (fieldType) {
+            longClass, integerClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, longClass).lt(value[0].toLong()))
+            doubleClass, floatClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, doubleClass).lt(value[0].toDouble()))
         }
     }),
     GOE("goe", { booleanBuilder, pathBuilder, propertyPath, value, fieldType ->
-        value.forEach {
-            when(fieldType){
-                longClass, integerClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, longClass).goe(it.toLong()))
-                doubleClass, floatClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, doubleClass).goe(it.toDouble()))
-            }
+        when (fieldType) {
+            longClass, integerClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, longClass).goe(value[0].toLong()))
+            doubleClass, floatClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, doubleClass).goe(value[0].toDouble()))
         }
     }),
     LOE("loe", { booleanBuilder, pathBuilder, propertyPath, value, fieldType ->
-        value.forEach {
-            when(fieldType){
-                longClass, integerClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, longClass).loe(it.toLong()))
-                doubleClass, floatClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, doubleClass).loe(it.toDouble()))
-            }
+        when (fieldType) {
+            longClass, integerClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, longClass).loe(value[0].toLong()))
+            doubleClass, floatClass -> booleanBuilder.and(pathBuilder.getNumber(propertyPath, doubleClass).loe(value[0].toDouble()))
         }
     }),
     CONTAINS("contains", { booleanBuilder, pathBuilder, propertyPath, value, _ ->
         value.forEach { booleanBuilder.and(pathBuilder.getString(propertyPath).containsIgnoreCase(it)) }
     }),
-    STARTS_WITH("startsWith", { booleanBuilder, pathBuilder, propertyPath, value, _ -> booleanBuilder.and(pathBuilder.getString(propertyPath).startsWith(value[0])) }
-
-    ),
+    STARTS_WITH("startsWith", { booleanBuilder, pathBuilder, propertyPath, value, _ ->
+        booleanBuilder.and(pathBuilder.getString(propertyPath).startsWith(value[0]))
+    }),
     IS_NULL("isNull", { booleanBuilder, pathBuilder, propertyPath, value, _ ->
         value.add("")
         booleanBuilder.and(pathBuilder.get(propertyPath).isNull)
@@ -68,19 +65,19 @@ enum class SearchOperator(val operator: String,
         booleanBuilder.and(pathBuilder.get(propertyPath).isNotNull)
     }),
     DATE_LESS_OR_EQUAL("dloe", { booleanBuilder, pathBuilder, propertyPath, value, fieldType ->
-        when(fieldType){
+        when (fieldType) {
             localDateClass -> booleanBuilder.and(pathBuilder.getDate(propertyPath, localDateClass).loe(LocalDate.parse(value[0])))
             localDateTimeClass -> booleanBuilder.and(pathBuilder.getDate(propertyPath, localDateTimeClass).loe(LocalDateTime.parse(value[0], DateTimeFormatter.ISO_LOCAL_DATE)))
         }
     }),
     DATE_GREATER_OR_EQUAL("dgoe", { booleanBuilder, pathBuilder, propertyPath, value, fieldType ->
-        when(fieldType){
+        when (fieldType) {
             localDateClass -> booleanBuilder.and(pathBuilder.getDate(propertyPath, localDateClass).goe(LocalDate.parse(value[0])))
             localDateTimeClass -> booleanBuilder.and(pathBuilder.getDate(propertyPath, localDateTimeClass).goe(LocalDateTime.parse(value[0], DateTimeFormatter.ISO_LOCAL_DATE)))
         }
     }),
     DATE_EQUAL("deq", { booleanBuilder, pathBuilder, propertyPath, value, fieldType ->
-        when(fieldType){
+        when (fieldType) {
             localDateClass -> booleanBuilder.and(pathBuilder.getDate(propertyPath, localDateClass).eq(LocalDate.parse(value[0])))
             localDateTimeClass -> booleanBuilder.and(pathBuilder.getDate(propertyPath, localDateTimeClass).eq(LocalDateTime.parse(value[0], DateTimeFormatter.ISO_LOCAL_DATE)))
         }
@@ -90,9 +87,9 @@ enum class SearchOperator(val operator: String,
             fieldType.isEnum -> {
                 val conversionService = DefaultConversionService()
                 booleanBuilder.and(pathBuilder.get(propertyPath).`in`(
-                        *values.map{conversionService.convert(it, fieldType)}.toTypedArray()))
+                        *values.map { conversionService.convert(it, fieldType) }.toTypedArray()))
             }
-            fieldType == longClass -> booleanBuilder.and(pathBuilder.get(propertyPath).`in`(*values.map{ it.toLong() }.toTypedArray()))
+            fieldType == longClass -> booleanBuilder.and(pathBuilder.get(propertyPath).`in`(*values.map { it.toLong() }.toTypedArray()))
             else -> booleanBuilder.and(pathBuilder.get(propertyPath).`in`(*values.toTypedArray()))
         }
     });
---
+++ api/src/test/java/api/config/auditor/AuditAwareTest.kt
@@ -0,0 +1,47 @@
+package api.config.auditor
+
+import api.entity.User
+import api.repository.UserRepository
+import api.security.model.JwtAuthenticationToken
+import api.security.model.JwtUserDetails
+import com.nhaarman.mockito_kotlin.whenever
+import org.amshove.kluent.`should be`
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.springframework.security.core.context.SecurityContextHolder
+import java.util.*
+
+@RunWith(MockitoJUnitRunner::class)
+class AuditAwareTest {
+
+    @InjectMocks
+    lateinit var testSubject: AuditAwareImpl
+
+    @Mock
+    lateinit var userRepository: UserRepository
+
+    @Before
+    fun setUp() {
+        val userDetails = JwtUserDetails().apply { username = "user" }
+        SecurityContextHolder.getContext().authentication =
+                JwtAuthenticationToken(userDetails, null, null, null)
+    }
+
+    @Test
+    fun `should return logged user from context`() {
+
+        //given
+        val user = User().apply { username = "user" }
+        whenever(userRepository.findByUsername("user")).thenReturn(Optional.of(user))
+
+        //when
+        val result = testSubject.currentAuditor.get()
+
+        //then
+        result `should be` user
+    }
+}
